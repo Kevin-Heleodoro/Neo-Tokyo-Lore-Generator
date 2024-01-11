956c8c468045ec6bf126f50654d412c0024fdd43
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2024-01-10 21:56:48 -0500

    -   Fixed the alchemyDataService in Frontend. I needed to set the methods to  and then  the calls. -   Created first iteration of Header. -   Created first iteration of NftCardContainer and NftCard -   Added the getCitizenForWallet route to alchemy.js -   Need to work on what I want the app to actually look like. Side navbar or top header? Do I even need a header? The MVP is to allow a citizen to connect their wallet, verify that they have a citizen NFT, grant access to lore generator, use chatgpt to create a background for the citizen based on the nft's traits, then allow the citizen to share on twittersepia filter

--- backend/api/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { NFTController } = require('./controllers/nftController.js');
-
-const router = express.Router();
-
-router.route('/nfts/:address').get(NFTController.apiGetNFTsForOwner);
-router.route('/citizen/:address').get(NFTController.apiGetOwnersCitizen);
-
-module.exports = { router };
