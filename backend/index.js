99173ac5d36e2fa3798244e1e75db0e8a18589ed
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2024-05-25 18:43:25 -0400

    Added cors, security, and error handling to backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 const dotenv = require('dotenv');
-// const { Alchemy, Network } = require('alchemy-sdk');
+const helmet = require('helmet');
+const compression = require('compression');
+
 const { app } = require('./server.js');
 
+let envFile =
+    process.env.NODE_ENV === 'production' ? '.env' : '.env.development';
+dotenv.config({ path: envFile });
+
+app.use(helmet());
+app.use(compression());
+
 async function main() {
-    dotenv.config({ path: '.env.development' });
     const port = process.env.SERVER_PORT || 8082;
 
     try {
@@ -14,8 +22,28 @@ async function main() {
         console.error(e);
         process.exit(1);
     }
+
+    // Graceful shutdown
+    process.on('SIGTERM', () => {
+        console.info('SIGTERM signal received. Closing HTTP server.');
+        app.close(() => {
+            console.info('HTTP server closed.');
+            process.exit(0);
+        });
+    });
+
+    process.on('SIGINT', () => {
+        console.info('SIGINT signal received. Closing HTTP server.');
+        app.close(() => {
+            console.info('HTTP server closed.');
+            process.exit(0);
+        });
+    });
 }
 
-main().catch(console.error);
+main().catch((e) => {
+    console.error('Unhandled promise rejection. Exiting process.', e);
+    process.exit(1);
+});
 
 module.exports = { app };
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+
 const { router } = require('./api/router.js');
 
 const app = express();
+
+const whitelist = [process.env.REACT_APP_BASE_URL];
+if (process.env.NODE_ENV === 'development') {
+    whitelist.push('http://localhost:3000');
+}
+
 const options = {
-    origin: '*',
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     optionSuccessStatus: 200,
 };
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests from this IP, please try again later!',
+});
+
+app.use(limiter);
 app.use(cors(options));
 app.use(express.json());
 
@@ -16,5 +37,10 @@ app.use('/api/v1', router);
 app.use('*', (req, res) =>
     res.status(404).json({ error: 'Nice try meatbag ...' })
 );
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+});
 
 module.exports = { app };
